d300e1733fc62ba7edfa4c627573cd6902ed379f
wizgurl101 <nb.phan@hotmail.com>
2024-12-16 20:16:15 -0700

    Update LoginScreen.kt

--- app/src/main/java/com/example/mybudgetyandroidapp/LoginScreen.kt
+++ app/src/main/java/com/example/mybudgetyandroidapp/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.mybudgetyandroidapp
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,15 +12,22 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+
 
 @Composable
 fun LoginScreen(onLoginSuccess: () -> Unit) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     val logoPainter = painterResource(id = R.drawable.my_budgety_logo)
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -33,23 +41,38 @@ fun LoginScreen(onLoginSuccess: () -> Unit) {
             modifier = Modifier.size(200.dp)
         )
 
+        Spacer(modifier = Modifier.height(4.dp))
+
         Text(text = "My Budgety")
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        OutlinedTextField(value = "",
-            onValueChange = { /*TODO*/ },
-            label = {Text(text = "Email")})
+        OutlinedTextField(value = email,
+            onValueChange = {
+                email = it
+            },
+            label = {
+                Text(text = "Email")
+            })
 
         Spacer(modifier = Modifier.height(4.dp))
 
-        OutlinedTextField(value = "",
-            onValueChange = { /*TODO*/ },
-            label = {Text(text = "Password")})
+        OutlinedTextField(value = password,
+            onValueChange = {
+                password = it
+            },
+            label = {
+                Text(text = "Password")
+            },
+            visualTransformation = PasswordVisualTransformation(),
+        )
 
         Spacer(modifier = Modifier.height(6.dp))
 
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = {
+        /*TODO: implement login logic for now we are logging it*/
+        Log.i("Login", "Email: $email, Password: $password")
+        }) {
             Text(text = "Login")
         }
     }
