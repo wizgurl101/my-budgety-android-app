27f39b96a46dc68d1a9983251a1ed18d95e358ca
wizgurl101 <nb.phan@hotmail.com>
2024-12-20 01:50:05 -0700

    change the color scheme

--- app/src/main/java/com/example/mybudgetyandroidapp/LoginScreen.kt
+++ app/src/main/java/com/example/mybudgetyandroidapp/LoginScreen.kt
@@ -2,6 +2,7 @@ package com.example.mybudgetyandroidapp
 
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,18 +24,25 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 
 @Composable
 fun LoginScreen(onLoginSuccess: () -> Unit) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    val customGreenColor = Color(0xFF507c5c)
+    val customBackgroundColor = Color(0xFFF7F7F7)
 
     val logoPainter = painterResource(id = R.drawable.my_budgety_logo)
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(customBackgroundColor),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
     )
     {
         Image(
@@ -43,7 +53,14 @@ fun LoginScreen(onLoginSuccess: () -> Unit) {
 
         Spacer(modifier = Modifier.height(4.dp))
 
-        Text(text = "My Budgety")
+        Text(
+            text = "My Budgety",
+            style = MaterialTheme.typography.bodyLarge.copy(
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = customGreenColor
+            )
+        )
 
         Spacer(modifier = Modifier.height(8.dp))
 
@@ -69,10 +86,14 @@ fun LoginScreen(onLoginSuccess: () -> Unit) {
 
         Spacer(modifier = Modifier.height(6.dp))
 
-        Button(onClick = {
+        Button(
+            onClick = {
         /*TODO: implement login logic for now we are logging it*/
         Log.i("Login", "Email: $email, Password: $password")
-        }) {
+        },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = customGreenColor
+            )) {
             Text(text = "Login")
         }
     }
